0ec1d38c17c5dc9e6ec5cefb1a2abbbd6380285d
Ramon Oliveira <ramonoliveiradias27@gmail.com>
2023-08-14 13:34:33 -0300

    Fix create notice page, create tooglecheckbox comp

--- src/app/(dash)/news/create/page.tsx
+++ src/app/(dash)/news/create/page.tsx
@@ -1,65 +1,41 @@
 
+import ToogleCheckbox from '@/app/components/toogleCheckbox';
 import Button from '../../../components/button'
 import CircleIcon from "../../../components/circleIcon";
 
-export default function CreateNews(){
+export default function CreateNews() {
 
-    
+
     return (
-        <div className="overflow-hidden relative h-screen bg-backgroundPrimary">
-                <div className="p-8 w-full rounded-2xl text-textPrimary bg-gradientBox font-raleway flex flex-col" >
-                    <div className='flex flex-row justify-between'>
-                        <div className='flex flex-row gap-4 items-center'>
-                        <CircleIcon icon="rocket" />
-                            <h1 className="text-textPrimary text-3xl font-bold ">Adicionar Notícia </h1>
-                        </div>
-                    </div>
-                </div>
-                
-                <div className='container bg-gradientBox'>
-                    <div className='flex flex-col lg:flex-row bg-gradientBox'>
-                        <div className="ml-8 p-8 w-full md:w-80 rounded-2xl text-textPrimary  font-raleway flex flex-col lg:flex-row" >
-                            <div className="input-group mt-2 lg:mt-0 lg:mr-2 flex-grow">
-                                <label className="text-textPrimary ml-4">Título</label>
-
-                                <div className="relative flex items-center ">
-                                    <i className="fa-solid fa-pen w-5 h-1 absolute ml-3 fill-transparent"/>
-                                    <input type="text" className="input-text-new mt-1 w-full lg:w-96" placeholder="Título"/>
-                                </div>
-
-                            </div>                    
-                        </div>
-                        <div className="flex flex-row ml-96 gap-4 items-center">
-                                <label className="input-label h-2">Notícia Ativa?</label>
-                                <label className="relative inline-flex items-center mr-5 cursor-pointer mt-4">
-                                    <input type="checkbox" value="" className="sr-only peer"/>
-                                    <div className="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-green-300 dark:peer-focus:ring-green-800 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-[#077E71]"></div>
-                                    <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300"></span>
-                                </label>
-                               
-                        </div>  
-                    </div>
-                
-                   
-
-                    <div className="flex flex-col lg:flex-row bg-gradientBox" >
-                        <div className="ml-8 p-8 w-full  md:w-130 rounded-2xl text-textPrimary  font-raleway flex flex-col lg:flex-row" >
-                            <div className="input-group mt-2 lg:mt-0 lg:mr-2 flex-grow">
-                                <textarea  className="input-text-new md:h-80"/>
-                            </div>
-                         </div>
-
-                    </div>
-                        <div className="ml-8 p-82 w-full md:w-802 rounded-2xl font-raleway flex flex-col md:flex-row justify-center">
-                            <Button fullWidth pattern={1} className="h-10 mt-8 w-80"> <i className="fa-brands fa-telegram text-textPrimary flex justify-content align-items w-10" />Cadastrar Notícia</Button>
-                        </div>
-                        
-                </div>
-                    
-                
+        <div className='flex flex-col min-h-screen h-auto'>
+            <div className="flex flex-row gap-4 items-center">
+                <CircleIcon icon="rocket" />
+                <h1 className="text-lg text-textPrimary">Adicionar notícia</h1>
             </div>
-            
-            
+            <div className="box-general" >
+                <div className='flex flex-col lg:flex-row bg-gradientBox items-center justify-between'>
+                    <div className=" w-full md:w-80 rounded-2xl text-textPrimary  font-abeezee flex flex-col lg:flex-row" >
+                        <div className="input-group mt-2 lg:mt-0 lg:mr-2 flex-grow">
+                            <label className="text-textPrimary ml-4">Título</label>
 
+                            <div className="relative flex items-center ">
+                                <i className="fa-solid fa-pen w-5 h-1 absolute ml-3 fill-transparent" />
+                                <input type="text" className="input-text-new mt-1 w-full lg:w-96 h-8" placeholder="Título" />
+                            </div>
+                        </div>
+                    </div>
+                    <ToogleCheckbox title='Noticia ativa ?' checked={true} function={undefined} />
+                </div>
+                <div className=" w-full md:w-130 rounded-2xl text-textPrimary font-abeezee flex flex-col lg:flex-row" >
+                    <div className="input-group mt-2 lg:mt-0 lg:mr-2 flex-grow">
+                        <textarea className="input-text-new md:h-80" />
+                    </div>
+                </div>
+                <div className=" mt-8 w-full rounded-2xl font-abeezee flex flex-col md:flex-row justify-end">
+                    <Button fullWidth pattern={1} className="h-10 float-right rounded-md text-sm px-6 flex flex-row gap-x-4 items-center"> <i className="fa-brands fa-telegram text-textPrimary flex justify-content align-items" /><h1 className='text-textPrimary text-sm'>Cadastrar Notícia</h1></Button>
+                </div>
+
+            </div>
+        </div>                       
     )
 }
---
+++ src/app/components/toogleCheckbox.tsx
@@ -0,0 +1,31 @@
+type toogleCheckBoxProps = {
+    title: string,
+    function: any,
+    checked: any
+}
+export default function ToogleCheckbox(props: toogleCheckBoxProps){
+    return (
+        <div className='flex flex-row items-center'>
+            <h1 className='font-abeezee text-sm my-auto'>{props.title}</h1>
+            <div className=" relative ml-2 mt-1 mr-2 inline-block w-8 select-none align-middle transition duration-200 ease-in my-auto">
+                <input
+                    type="checkbox"
+                    name={`toogle`}
+                    id={`toogle`}
+                    onClick={props.function != undefined ? props.function : undefined}
+                    defaultChecked={props.checked}
+                    checked={props.checked}
+                    className={`toggle-checkbox absolute block h-4 w-4 cursor-pointer appearance-none rounded-full border-4 border-red-800 bg-red-800 transition-all duration-500 checked:right-0 checked:border-textGreen checked:bg-textGreen`}
+                />
+                <label
+                    htmlFor={`toogle`}
+                    className={`toggle-label block h-4 cursor-pointer overflow-hidden rounded-full border-2 border-solid border-grayLight2 transition-all duration-500 ${
+                        props.checked
+                            ? "bg-textGreen/60"
+                            : "bg-red-800/60"
+                    }`}
+                ></label>
+            </div>
+        </div>
+    )
+}
